d3f12b69ea1dfc597b24deb47e6c181471698e44
lizzie lerwill <lizzielerwill@hotmail.com>
2022-04-26 16:37:37 +0200

    changed home to capital

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import Home from './Components/home';
+import Home from './Components/Home';
 import About from './Components/About';
 import Skills from './Components/Skills';
 import Work from './Components/Work';
