2c698b42bc6d625e9bf64168667c3546c0e027bc
lizzie lerwill <lizzielerwill@hotmail.com>
2022-04-27 23:46:11 +0200

    corrected skills overflow

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -14,12 +14,11 @@ const Skills = () => {
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
       {/*Container*/}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div>
+        <div className='pb-5'>
           <p className='text-4xl font-bold inline border-b-4 border-[#d3877e]'>Skills</p>
-          <p className='py-4 text-xl'>These are the technologies I've used:</p>
         </div>
 
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-4'>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-2'>
           <div className='shadow-[0_3px_10px_rgb(0,0,0,0.2)] shadow-[#679186] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto pt-3' src={HTML} alt="HTML icon" />
             <p>HTML</p>
