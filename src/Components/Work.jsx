fcf1133a3cad17d31eaf5fb71706f08d5af8d631
lizzie lerwill <lizzielerwill@hotmail.com>
2022-05-04 09:59:07 +0200

    changed typo in work

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -12,7 +12,7 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#0a192f]'>Work</p>
-          <p className='text-xl py-6'>Check out my some of my work:</p>
+          <p className='text-xl py-6'>Check out some of my work:</p>
         </div>
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
